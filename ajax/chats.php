a9d8ce087e7454d3fa94dcc45dc95fc8f2e682ec
adoletskiy <adoletsky@indigos.ru>
2018-05-12 00:11:47 +0300

    add CHATS

--- ajax/chats.php
+++ ajax/chats.php
@@ -1,6 +1,6 @@
 <?php
 function BXCRMwebhook($command, $data = array()){
-    $queryUrl = 'https://practicumonline.bitrix24.ru/rest/1/808gmadqp2pwcyb2/im.message.add.json';
+    $queryUrl = 'https://practicumonline.bitrix24.ru/rest/1/808gmadqp2pwcyb2/imopenlines.network.message.add.json';
     $queryData = http_build_query($data);
     $curl = curl_init();
     curl_setopt_array($curl, array(CURLOPT_SSL_VERIFYPEER => 0, CURLOPT_POST => 1, CURLOPT_HEADER => 0, CURLOPT_RETURNTRANSFER => 1, CURLOPT_URL => $queryUrl, CURLOPT_POSTFIELDS => $queryData,));
@@ -10,9 +10,9 @@ function BXCRMwebhook($command, $data = array()){
     return $result;
 }
 $arData=array(
-    'CODE' => '808gmadqp2pwcyb2',
-    'CHAT_ID' => 22, // Идентификатор чата получателя, если сообщение для чата
-    'MESSAGE' => 'Текст сообщения' // Тест сообщения
+    'CODE' => '9e9b79fa2f4a2f27abf5551eba2ee8fb',
+    'USER_ID' => 2, // Идентификатор чата получателя, если сообщение для чата
+    'MESSAGE' => 'Приветсвуем Вас у нас на курсах!' // Тест сообщения
 );
 $arResult=BXCRMwebhook($command, $arData);
 ?><pre><?print_r($arResult)?></pre>
