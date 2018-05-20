f43a7db7ba769104e9ea10e73826e2d0ada7bf13
adoletskiy <adoletsky@indigos.ru>
2018-05-20 15:39:24 +0300

    style pages auth

--- index.php
+++ index.php
@@ -2,27 +2,37 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Авторизация");
 ?>
+    <div class="section-content mfp-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <?
+                    if ($USER->IsAuthorized()) {
+                        if ($USER->IsAdmin()) {
+                            LocalRedirect('/lk/admin.php');
+                        } else {
+                            LocalRedirect('/lk/');
+                        }
+                    } else {
+                        $APPLICATION->IncludeComponent(
+                            "bitrix:system.auth.form",
+                            "",
+                            Array(
+                                "REGISTER_URL" => "",
+                                "FORGOT_PASSWORD_URL" => "/fogotpass.php",
+                                "PROFILE_URL" => "",
+                                "SHOW_ERRORS" => "Y"
+                            ),
+                            false
+                        );
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<?
-if($USER->IsAuthorized()){
-    if($USER->IsAdmin()){
-        LocalRedirect('/lk/admin.php');
-    }else{
-        LocalRedirect('/lk/');
-    }
-}else{
-    $APPLICATION->IncludeComponent(
-        "bitrix:system.auth.form",
-        "",
-        Array(
-            "REGISTER_URL" => "",
-            "FORGOT_PASSWORD_URL" => "/fogotpass.php",
-            "PROFILE_URL" => "",
-            "SHOW_ERRORS" => "Y"
-        ),
-        false
-    );
-}
-?>
+
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
