71be52e519977e4f2b04b55360950dfc5974525f
doletsky <doletsky@bk.ru>
2018-07-31 14:26:46 +0300

    debug template student

--- urlrewrite.php
+++ urlrewrite.php
@@ -17,7 +17,25 @@ $arUrlRewrite = array(
 		"RULE"	=>	"tab=$1",
 		"ID"	=>	"",
 		"PATH"	=>	"/lk/profile/index.php",
-	)
+	),
+    array(
+        "CONDITION"	=>	"#^/student/course/([0-9a-zA-Z_-]+)/([0-9a-zA-Z_-]+)/.*#",
+        "RULE"	=>	"course=$1&id=$2",
+        "ID"	=>	"",
+        "PATH"	=>	"/student/index.php",
+    ),
+    array(
+        "CONDITION"	=>	"#^/student/course/([0-9a-zA-Z_-]+)/.*#",
+        "RULE"	=>	"course=$1",
+        "ID"	=>	"",
+        "PATH"	=>	"/student/index.php",
+    ),
+    array(
+        "CONDITION"	=>	"#^/student/profile/([0-9a-zA-Z_-]+)/.*#",
+        "RULE"	=>	"tab=$1",
+        "ID"	=>	"",
+        "PATH"	=>	"/student/profile/index.php",
+    )
 );
 
 ?>
